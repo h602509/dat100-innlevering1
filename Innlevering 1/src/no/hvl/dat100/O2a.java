86acfa4d3e0f9d059172f05d21b2ac7a2e01633b
h602509 <h602509@stud.hvl.no>
2021-09-04 12:25:24 +0200

    opplasting O2

---
+++ Innlevering 1/src/no/hvl/dat100/O2a.java
@@ -0,0 +1,28 @@
+package no.hvl.dat100;
+
+import static javax.swing.JOptionPane.showInputDialog;
+
+public class O2a {
+
+	public static void main(String[] args) {
+
+		String poengsumTxt = showInputDialog("Oppgi poengsum");
+		int poengsum = Integer.parseInt(poengsumTxt);
+
+		if (poengsum < 0 || poengsum >= 101) {
+			System.out.println("pengsum er ugylding");	
+		} else if (poengsum <= 39) {
+			System.out.println("poengsummen " + poengsum + " karakter: F");
+		} else if (poengsum <= 49) {
+			System.out.println("poengsummen " + poengsum + " karakter: E");
+		} else if (poengsum <= 59) {
+			System.out.println("poengsummen " + poengsum + " karakter: D");
+		} else if (poengsum <= 79) {
+			System.out.println("poengsummen " + poengsum + " karakter: C");
+		} else if (poengsum <= 89) {
+			System.out.println("poengsummen " + poengsum + " karakter: B");
+		} else {
+			System.out.println("poengsummen " + poengsum + " karakter: A");
+		}
+	}
+}
